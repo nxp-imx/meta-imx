4d18f24d6d7ece0235bc5383b14b3cfcbd9846e4
Tom Hochstein <tom.hochstein@nxp.com>
2017-09-20 10:46:17 -0500

    tools/utils.sh: Extend hook_in_layer
    Extend hook_in_layer to allow overriding an arbitrary list
    of upstream layers.
    
    Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>

--- imx/tools/utils.sh
+++ imx/tools/utils.sh
@@ -13,29 +13,32 @@ file_override() {
 
 machine_overrides() {
     layer=$1
+    upstream_layer=$2
     machines="../sources/$layer/conf/machine/*"
     machine_includes="../sources/$layer/conf/machine/include/*"
     for machine in $machines; do
-        file_override $machine ../sources/meta-freescale/conf/machine
+        file_override $machine ../sources/$upstream_layer/conf/machine
     done
     for machine_include in $machine_includes; do
-        file_override $machine_include ../sources/meta-freescale/conf/machine/include
+        file_override $machine_include ../sources/$upstream_layer/conf/machine/include
     done
 }
 
 bbclass_overrides() {
     layer=$1
+    upstream_layer=$2
     bbclasses="../sources/$layer/classes/*"
     for bbclass in $bbclasses; do
-        file_override $bbclass ../sources/meta-freescale/classes
+        file_override $bbclass ../sources/$upstream_layer/classes
     done
 }
 
-# hook_in_layer
+# hook_in_layer LAYER-NAME [UPSTREAM-LAYER-NAME]...
 #
-# Adds the specified layer $1 to bblayers.conf. Also takes care of
-# any machine and/or bbclass files in the layer that are overriding a
-# file in meta-freescale. The function is expected to be called
+# Adds the specified layer to bblayers.conf. Also takes care of
+# any machine and/or bbclass files in the layer that are overriding
+# files in an upstream layer. If no upstream layer is specified,
+# meta-freescale is used. The function is expected to be called
 # from the build folder.
 #
 # The specified layer is defined as the path from the sources folder
@@ -44,8 +47,18 @@ bbclass_overrides() {
 # Example:
 #   hook_in_layer meta-fsl-bsp-release/imx/meta-bsp
 hook_in_layer() {
+
     layer=$1
+    shift
+    if [ "$1" = "" ]; then
+        upstream_layers = "meta-freescale"
+    else
+        upstream_layers = "$@"
+    fi
+
     echo "BBLAYERS += \" \${BSPDIR}/sources/$layer \"" >> conf/bblayers.conf
-    machine_overrides $layer
-    bbclass_overrides $layer
+    for upstream_layer in upstream_layers; do
+        machine_overrides $layer $upstream_layer
+        bbclass_overrides $layer $upstream_layer
+    done
 }
