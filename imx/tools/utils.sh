4827875dc527dcd7c5e8f7757490f6a0ffcdd632
Tom Hochstein <tom.hochstein@nxp.com>
2017-09-21 12:48:40 -0500

    tools/utils.sh: Fix build break
    Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>

--- imx/tools/utils.sh
+++ imx/tools/utils.sh
@@ -57,7 +57,7 @@ hook_in_layer() {
     fi
 
     echo "BBLAYERS += \" \${BSPDIR}/sources/$layer \"" >> conf/bblayers.conf
-    for upstream_layer in upstream_layers; do
+    for upstream_layer in $upstream_layers; do
         machine_overrides $layer $upstream_layer
         bbclass_overrides $layer $upstream_layer
     done
