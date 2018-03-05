9649b3ceda151a7666fd461bd7aecebfc47c9c98
Tom Hochstein <tom.hochstein@nxp.com>
2018-03-05 13:53:33 -0600

    setup: Cleanup setup utilities script [YOCIMX-2301]
    Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>

--- imx/tools/fsl-setup-release.sh
+++ imx/tools/fsl-setup-release.sh
@@ -18,7 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-. sources/meta-fsl-bsp-release/imx/tools/utils.sh
+. sources/meta-fsl-bsp-release/imx/tools/setup-utils.sh
 
 CWD=`pwd`
 PROGNAME="setup-environment"
