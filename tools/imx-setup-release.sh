09c61fbc200b143882b94524e613c89f7c10b6e8
Neena Busireddy <neenareddy.busireddy@nxp.com>
2020-04-10 09:15:28 -0500

    imx-setup-release.sh: Add meta-clang to bblayers.conf as chromium is dependent on it [YOCIMX-4233]
    Signed-off-by: Neena Busireddy <neenareddy.busireddy@nxp.com>

--- tools/imx-setup-release.sh
+++ tools/imx-setup-release.sh
@@ -161,6 +161,7 @@ hook_in_layer meta-imx/meta-ml
 echo "" >> $BUILD_DIR/conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-browser\"" >> $BUILD_DIR/conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-rust\"" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-clang\"" >> $BUILD_DIR/conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-gnome\"" >> $BUILD_DIR/conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-networking\"" >> $BUILD_DIR/conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-openembedded/meta-filesystems\"" >> $BUILD_DIR/conf/bblayers.conf
