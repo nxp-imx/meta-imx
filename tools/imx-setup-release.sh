5da3b13089b7aa74af21347faae3fd3f82535b83
Neena Busireddy <neenareddy.busireddy@nxp.com>
2021-10-01 10:33:05 -0500

    imx-setup-release.sh: Include meta-virtualization always
    Including meta-virtualization only for mx8 machines is blocking users to use
    the same build folder for all the machines
    
    Signed-off-by: Neena Busireddy <neenareddy.busireddy@nxp.com>

--- tools/imx-setup-release.sh
+++ tools/imx-setup-release.sh
@@ -187,10 +187,7 @@ echo "BBLAYERS += \"\${BSPDIR}/sources/meta-qt5\"" >> $BUILD_DIR/conf/bblayers.c
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-python2\"" >> $BUILD_DIR/conf/bblayers.conf
 
 # Enable docker for mx8 machines
-
-if [[ $MACHINE == imx8* ]]; then
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-virtualization\"" >> conf/bblayers.conf
-fi
 
 if [ -d ../sources/meta-ivi ]; then
     echo -e "\n## Genivi layers" >> $BUILD_DIR/conf/bblayers.conf
