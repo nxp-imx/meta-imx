nxp-imx/meta-imx

commit f414b41ad7e3b6601e17fb3a3a369099013187d0

arm-compute-library: Upgrade to 22.05 version [YOCIMX-6433]
Signed-off-by: Neena Busireddy <neenareddy.busireddy@nxp.com>

meta-ml/recipes-libraries/arm-compute-library/arm-compute-library_22.05.bb
@@ -5,10 +5,10 @@ LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://LICENSE;md5=9598101cf48c5f479cfda9f3fc6fc566 \
                     file://include/half/LICENSE.txt;md5=fe7e5a4795c76b317919afd2d3da5983"
 
-SRCBRANCH = "imx_21.08"
+SRCBRANCH = "imx_22.05"
 ARM_COMPUTELIBRARY_SRC ?= "git://source.codeaurora.org/external/imx/arm-computelibrary-imx.git;protocol=https"
 SRC_URI = "${ARM_COMPUTELIBRARY_SRC};branch=${SRCBRANCH}"
-SRCREV = "a7a16943ab603e8d5697b872421a91151a57cc7d" 
+SRCREV = "9423a53fc8a10f4c97ffcff6a1f198106a1f1c14" 
 
 S = "${WORKDIR}/git"
 

commit 119dcee8b145120a0ac17101292401039c95a01b

arm-compute-library: Update arm-compute-library_22.05.bb to commit 9423a53
Update the license.

Fixes: f414b41ad arm-compute-library: Upgrade to 22.05 version [YOCIMX-6433]
Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>

meta-ml/recipes-libraries/arm-compute-library/arm-compute-library_22.05.bb
@@ -2,7 +2,7 @@
 SUMMARY = "The ARM Computer Vision and Machine Learning library"
 DESCRIPTION = "The ARM Computer Vision and Machine Learning library is a set of functions optimised for both ARM CPUs and GPUs."
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://LICENSE;md5=9598101cf48c5f479cfda9f3fc6fc566 \
+LIC_FILES_CHKSUM = "file://LICENSE;md5=f3c5879801d3cffc4ac2399f2b8e8ec5 \
                     file://include/half/LICENSE.txt;md5=fe7e5a4795c76b317919afd2d3da5983"
 
 SRCBRANCH = "imx_22.05"