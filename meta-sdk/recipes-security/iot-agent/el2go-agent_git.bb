nxp-imx/meta-imx

commit 8f6232e12a0fe39936f470ee23fb974329f9f4d3

el2go-agent: Add EdgeLock 2GO IoT Agent libraries [YOCIMX-7065]
Add EdgeLock 2GO new recipes to build the IoT Agent libraries

Depends on the:
   - Security Middleware (SMW) library.
   - mBedTLS 3.3 (version 3.3.0) with PSA support.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

meta-sdk/recipes-security/iot-agent/el2go-agent_git.bb
@@ -1,0 +1,37 @@
+# Copyright 2023 NXP
+SUMMARY = "NXP i.MX EdgeLock 2GO IoT Agent"
+DESCRIPTION = "NXP i.MX EdgeLock 2GO IoT Agent"
+SECTION = "base"
+LICENSE = "Apache-2.0"
+LIC_FILES_CHKSUM = "file://LICENSE;md5=175792518e4ac015ab6696d16c4f607e"
+
+DEPENDS = "smw el2go-agent-mbedtls"
+
+SRC_URI = "${EL2GO_AGENT_SRC};branch=${EL2GO_AGENT_SRCBRANCH}"
+
+EL2GO_AGENT_SRC ?= "git://github.com/NXP/el2go-agent;protocol=https"
+EL2GO_AGENT_SRCBRANCH = "master"
+
+SRCREV = "32241f0dba2899b701a0103bdee029ae7f7d9741"
+S = "${WORKDIR}/git"
+
+inherit cmake
+
+CFLAGS[unexport] = "1"
+CPPFLAGS[unexport] = "1"
+AS[unexport] = "1"
+LD[unexport] = "1"
+
+EXTRA_OECMAKE = " \
+    -DSSS_HAVE_HOSTCRYPTO_MBEDTLS=ON \
+    -DNXP_IOT_AGENT_HAVE_PSA_IMPL_SMW=ON \
+    -DSSS_HAVE_MBEDTLS_ALT_PSA=ON \
+    -DSSS_HAVE_HOST_LINUX_LIKE=ON \
+    -DCMAKE_SYSROOT=${STAGING_DIR_HOST} \
+    -DNXP_SMW_DIR=${STAGING_DIR_HOST}/usr/lib/cmake"
+
+CFLAGS:append = " -DMBEDTLS_SSL_MAX_CONTENT_LEN=4096"
+CFLAGS:append = " -DMBEDTLS_USE_PSA_CRYPTO"
+
+COMPATIBLE_MACHINE = "(imx-nxp-bsp)"
+

meta-sdk/recipes-security/iot-agent/el2go-agent-mbedtls_3.3.0.bb
@@ -18,6 +18,9 @@ SRC_URI = "git://github.com/ARMmbed/mbedtls.git;protocol=https;branch=developmen
 
 inherit cmake update-alternatives ptest python3native
 
+SRC_URI:append = "file://0001-crypto_struct-change-psa_key_attributes_t-layout.patch \
+                  file://0002-library-tls-disable-PSA-APIs-for-most-operations.patch"
+
 PACKAGECONFIG = "psa"
 PACKAGECONFIG[shared-libs] = "-DUSE_SHARED_MBEDTLS_LIBRARY=ON,-DUSE_SHARED_MBEDTLS_LIBRARY=OFF"
 PACKAGECONFIG[programs] = "-DENABLE_PROGRAMS=ON,-DENABLE_PROGRAMS=OFF"

meta-sdk/recipes-security/iot-agent/files/0001-crypto_struct-change-psa_key_attributes_t-layout.patch
@@ -1,0 +1,43 @@
+From a3175b70bcfb4dbfc518ff1234fd4112ad40ef7a Mon Sep 17 00:00:00 2001
+From: Ilie Halip <ilie.halip@nxp.com>
+Date: Tue, 30 May 2023 14:01:15 +0300
+Subject: [PATCH] crypto_struct: change psa_key_attributes_t layout
+
+mbedtls can allocate a psa_key_attributes_t structure then call PSA APIs which
+are implemented by another library. Change the structure layout so that in this
+case both libraries have the same definition.
+
+Upstream-Status: Inappropriate [i.MX specific]
+
+Signed-off-by: Ilie Halip <ilie.halip@nxp.com>
+
+---
+ include/psa/crypto_struct.h | 6 +++---
+ 1 file changed, 3 insertions(+), 3 deletions(-)
+
+diff --git a/include/psa/crypto_struct.h b/include/psa/crypto_struct.h
+index afba32502..9d89e4165 100644
+--- a/include/psa/crypto_struct.h
++++ b/include/psa/crypto_struct.h
+@@ -305,7 +305,7 @@ static inline struct psa_key_policy_s psa_key_policy_init( void )
+ 
+ /* The type used internally for key sizes.
+  * Public interfaces use size_t, but internally we use a smaller type. */
+-typedef uint16_t psa_key_bits_t;
++typedef size_t psa_key_bits_t;
+ /* The maximum value of the type used to represent bit-sizes.
+  * This is used to mark an invalid key size. */
+ #define PSA_KEY_BITS_TOO_LARGE          ( ( psa_key_bits_t ) -1 )
+@@ -341,10 +341,10 @@ typedef uint16_t psa_key_attributes_flag_t;
+ 
+ typedef struct
+ {
++    mbedtls_svc_key_id_t MBEDTLS_PRIVATE(id);
++    psa_key_lifetime_t MBEDTLS_PRIVATE(lifetime);
+     psa_key_type_t MBEDTLS_PRIVATE(type);
+     psa_key_bits_t MBEDTLS_PRIVATE(bits);
+-    psa_key_lifetime_t MBEDTLS_PRIVATE(lifetime);
+-    mbedtls_svc_key_id_t MBEDTLS_PRIVATE(id);
+     psa_key_policy_t MBEDTLS_PRIVATE(policy);
+     psa_key_attributes_flag_t MBEDTLS_PRIVATE(flags);
+ } psa_core_key_attributes_t;

commit f25bfcb4cb94c036b81aa50852e32504f1d6d445

el2go-agent: Fix source branch variable
Fix the source branch variable to be set outside recipe.

Fixes: 8f6232e12a0f ("el2go-agent: Add EdgeLock 2GO IoT Agent libraries [YOCIMX-7065]")

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

meta-sdk/recipes-security/iot-agent/el2go-agent_git.bb
@@ -10,7 +10,7 @@ DEPENDS = "smw el2go-agent-mbedtls"
 SRC_URI = "${EL2GO_AGENT_SRC};branch=${EL2GO_AGENT_SRCBRANCH}"
 
 EL2GO_AGENT_SRC ?= "git://github.com/NXP/el2go-agent;protocol=https"
-EL2GO_AGENT_SRCBRANCH = "master"
+EL2GO_AGENT_SRCBRANCH ?= "master"
 
 SRCREV = "32241f0dba2899b701a0103bdee029ae7f7d9741"
 S = "${WORKDIR}/git"